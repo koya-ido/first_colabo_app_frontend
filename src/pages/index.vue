e8ae0a96dfa00dde3758ef0d4a33beead92e71c7
koya-ido <koya.development0@gmail.com>
2024-05-30 00:50:30 +0900

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <h1 class="title">{{ title }}</h1>
+  </div>
+</template>
+<script setup lang="ts">
+  const title = ref<string>("Hello World!!");
+</script>
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .title {
+    font-weight: bold;
+    font-size: 32px;
+  }
+}
+</style>
